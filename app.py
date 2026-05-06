231eeb35bdc41bc3a7f1d0c6cc68d5362f8545db
malayaranjannayak96-png <malayaranjannayak96@gmail.com>
2026-05-06 11:59:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,4 @@
 import streamlit as st
-
-st.title("My AI App is Working 🚀")
-
-st.write("अगर ये दिख रहा है, तो app सही deploy हो गया 👍")
-import streamlit as st
 import requests
 
 st.title("AI Video Generator 🎥")
