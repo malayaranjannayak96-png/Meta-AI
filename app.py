fa9af41172f96b4f0eb6d22504f11b7ebc958591
malayaranjannayak96-png <malayaranjannayak96@gmail.com>
2026-05-06 12:37:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,8 +13,8 @@ from groq import Groq
 
 st.title("AI Chat App 🤖")
 
-client = Groq(api_key=st.secrets["GROQ_API_KEY"])
-GROQ_API_KEY = "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"
+client = Groq(api_key=st.secrets["GROQ_API_KEY"])GROQ_API_KEY = "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"
+
 user_input = st.text_input("Ask something:")
 
 if st.button("Ask"):
